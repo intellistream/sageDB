8655d25d8d269f37024e9d21ece555bb982b7ca4
shuhao zhang <shuhao_zhang@hust.edu.cn>
2025-09-27 01:18:21 +0800

    clean

--- python/__init__.py
+++ python/__init__.py
@@ -7,6 +7,6 @@ try:
     from sage.middleware._version import __author__, __email__, __version__
 except ImportError:
     # 备用硬编码版本
-    __version__ = "0.1.3.1"
+    __version__ = "0.1.4"
     __author__ = "IntelliStream Team"
     __email__ = "shuhao_zhang@hust.edu.cn"
